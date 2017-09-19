f505b33909fecd7125e66d69ba3f429424ecd54b
KrokodilD <daniil_-91@mail.ru>
2017-09-19 15:12:55 +0200

    first commit

---
+++ src/FillpdfController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniil Krok
+ * Date: 19.09.2017
+ */
+
+namespace Daniilkrok\Fillpdf;
+
+use App\Http\Controllers\Controller;
+
+class FillpdfController extends Controller
+{
+
+    public function index()
+    {
+        echo 'test';
+    }
+
+}
---
+++ src/routes.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniil Krok
+ * Date: 19.09.2017
+ */
+
+Route::get('fillpdf/',
+    'daniilkrok\fillpdf\FillpdfController@index');
